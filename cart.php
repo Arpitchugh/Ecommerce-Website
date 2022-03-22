81da30379a9f9cad15f1acb821455801947c5fac
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-03-22 09:39:13 +0530

    code conversion from html to php

---
+++ cart.php
@@ -0,0 +1,22 @@
+<?php
+    
+    include ('header.php');
+?>
+
+<?php
+
+    /*  include products area  */
+        include ('Template/_cart-template.php');
+    /*  include products area  */
+
+    /*  include new phone section */
+        include ('Template/_new-phones.php');
+    /*  include new phone section */
+
+
+?>
+
+
+<?php
+include ('footer.php');
+?>
---
+++ product.php
@@ -0,0 +1,22 @@
+<?php
+    
+    include ('header.php');
+?>
+
+<?php
+
+    /*  include products area  */
+        include ('Template/_products.php');
+    /*  include products area  */
+
+    /*  include top sale section */
+        include ('Template/_top-sale.php');
+    /*  include top sale section */
+
+
+?>
+
+
+<?php
+include ('footer.php');
+?>
